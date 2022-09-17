fff0ab93afa7216c3b907d19d3a2c1d185608ba3
TheFairyTale <986920241@qq.com>
2022-09-17 12:18:26 +0800

    config static resource mapping

--- src/main/java/asia/dreamdropsakura/reggie/config/WebMvcConfig.java
+++ src/main/java/asia/dreamdropsakura/reggie/config/WebMvcConfig.java
@@ -1,10 +1,24 @@
 package asia.dreamdropsakura.reggie.config;
 
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
 /**
  * SpringMVC 配置类
  *
  * @author 童话的爱
  * @since 2022-9-17
  */
-public class WebMvcConfig {
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+
+    /**
+     * 添加静态资源映射
+     *
+     * @param registry
+     */
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/static/backend/");
+        registry.addResourceHandler("/frontend/**").addResourceLocations("classpath:/static/frontend/");
+    }
 }
