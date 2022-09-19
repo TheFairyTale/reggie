8edaffbf9ea02f7b9cfd75c2a1edb77768827f84
TheFairyTale <986920241@qq.com>
2022-09-19 16:52:06 +0800

    add pagination and list category feature.

--- src/main/java/asia/dreamdropsakura/reggie/controller/CategoryController.java
+++ src/main/java/asia/dreamdropsakura/reggie/controller/CategoryController.java
@@ -1,10 +1,13 @@
 package asia.dreamdropsakura.reggie.controller;
 
+import asia.dreamdropsakura.reggie.common.Result;
+import asia.dreamdropsakura.reggie.entity.Category;
 import asia.dreamdropsakura.reggie.service.CategoryService;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 分类管理相关控制器类
@@ -19,4 +22,47 @@ public class CategoryController {
 
     @Autowired
     private CategoryService categoryService;
+
+    /**
+     * 分页获取所有套餐与菜品
+     *
+     * @param page 当前页数
+     * @param pageSize 每页记录数
+     * @return
+     */
+    @GetMapping("/page")
+    public Result<IPage> getCategories(int page, int pageSize) {
+        Page<Category> categoryPage = new Page<>(page, pageSize);
+        Page<Category> page1 = categoryService.page(categoryPage);
+        return Result.success(page1);
+    }
+
+    /**
+     * 新增套餐、菜品接口
+     *
+     * @param category
+     * @return
+     */
+    @PostMapping
+    public Result<String> addCategory(@RequestBody Category category) {
+        if (category != null) {
+            categoryService.save(category);
+            return Result.success("新增成功");
+        }
+        return Result.error("新增失败, 内容为空, 请重试");
+    }
+
+    /**
+     * 删除套餐、菜品接口
+     *
+     */
+    @DeleteMapping
+    public Result<String> deleteCategory(@RequestParam long id) {
+        categoryService.removeById(id);
+        return Result.success("删除成功");
+    }
+
 }
+
+
+
