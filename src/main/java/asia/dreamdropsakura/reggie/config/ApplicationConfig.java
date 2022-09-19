12972846ca2e2267550394d5f06dd2124514ed22
TheFairyTale <986920241@qq.com>
2022-09-19 10:56:54 +0800

    create SpringBoot configuration class used to add mybatis-plus pagination plugin

---
+++ src/main/java/asia/dreamdropsakura/reggie/config/ApplicationConfig.java
@@ -0,0 +1,27 @@
+package asia.dreamdropsakura.reggie.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * SpringBoot 应用程序配置类
+ *
+ * @author 童话的爱
+ * @since 2022-9-18
+ */
+@Configuration
+public class ApplicationConfig {
+    /**
+     * Mybatis-Plus 分页插件
+     *
+     * @return
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return interceptor;
+    }
+}
