c923f08bb34e754bea1f6ac2d64419a0ec35634e
TheFairyTale <986920241@qq.com>
2022-09-19 10:54:48 +0800

    add employee CRUD restful method

--- src/main/java/asia/dreamdropsakura/reggie/controller/EmployeeController.java
+++ src/main/java/asia/dreamdropsakura/reggie/controller/EmployeeController.java
@@ -4,16 +4,17 @@ import asia.dreamdropsakura.reggie.common.Result;
 import asia.dreamdropsakura.reggie.entity.Employee;
 import asia.dreamdropsakura.reggie.service.EmployeeService;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.DigestUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
 
 /**
  * 员工相关控制器类
@@ -31,6 +32,7 @@ public class EmployeeController {
 
     /**
      * 员工登录
+     *
      * 员工登录的处理逻辑如下
      * 1. 将前端页面提交的密码password 进行md5 加密
      * 2-3. 根据页面提交的用户名username 密码password 查询数据库
@@ -62,7 +64,6 @@ public class EmployeeController {
             return Result.error("登陆失败，该用户已被禁用。");
         }
         // 6
-        System.out.println("让我看看你的pwd: " + one.getPassword());
         HttpSession session = request.getSession();
         //session.setAttribute(one.getUsername(), one.getId());
         session.setAttribute("employee", one.getId());
@@ -80,5 +81,67 @@ public class EmployeeController {
         // todo 前端不传值怎么知道要登出哪个用户？
         request.getSession().getAttribute("employee");
         return Result.success("登出成功.");
+    }
+
+    /**
+     * 新增员工
+     *
+     * @param employee 前端传来的值
+     * @param servletRequest 为了获得当前登录的用户的session
+     */
+    @PostMapping
+    public Result<Employee> addEmployee(HttpServletRequest servletRequest , @RequestBody Employee employee) {
+        if (employee != null && employee.getUsername() != null) {
+            // 设置新用户的初始密码
+            employee.setPassword(DigestUtils.md5DigestAsHex("123456".getBytes()));
+            // 设置该新增用户的新增时间
+            employee.setCreateTime(LocalDateTime.now());
+            employee.setUpdateTime(LocalDateTime.now());
+            // 设置该新增用户为哪位用户新增的
+            Object employee1 = servletRequest.getSession().getAttribute("employee");
+            employee.setUpdateUser((Long) employee1);
+            employee.setCreateUser((Long) employee1);
+
+            // 提交
+            employeeService.save(employee);
+            return Result.success("成功");
+        }
+        return Result.error("输入的内容不能为空");
+    }
+
+    /**
+     * 分页查询员工
+     *
+     * @param name 员工姓名，可选参数
+     * @param page 当前页数
+     * @param pageSize 每页记录数
+     */
+    @GetMapping("/page")
+    public Result<IPage> getEmployees(int page, int pageSize, String name) {
+        IPage<Employee> employeePage = new Page<>(page, pageSize);
+        IPage<Employee> page1 = employeeService.page(employeePage, new LambdaQueryWrapper<Employee>().like(name != null, Employee::getName, name).orderByAsc(Employee::getUpdateTime));
+        return Result.success(page1);
+    }
+
+    /**
+     * 封禁指定员工账号, 另外也可用于修改员工信息(因为传入的是整个Employee 对象)
+     *
+     */
+    @PutMapping
+    public Result<String> prohibitEmployee(HttpServletRequest httpServletRequest, @RequestBody Employee employee) {
+        employee.setUpdateTime(LocalDateTime.now());
+        employee.setUpdateUser((Long) httpServletRequest.getSession().getAttribute("employee"));
+        employeeService.update(employee, new QueryWrapper<Employee>().eq("id", employee.getId()));
+        return Result.success("成功");
+    }
+
+    /**
+     * 获取指定id 的员工
+     *
+     */
+    @GetMapping("/{id}")
+    public Result<Employee> getEmployeeById(@PathVariable long id) {
+        Employee byId = employeeService.getById(id);
+        return Result.success(byId);
     }
 }
