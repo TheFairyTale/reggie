2ee01980b3e14b861da172aad05f086f4cae4cc1
TheFairyTale <986920241@qq.com>
2022-09-26 18:22:04 +0800

    新增前端点餐界面资源放行与验证是否登录功能

--- src/main/java/asia/dreamdropsakura/reggie/filter/LoginCheckFilter.java
+++ src/main/java/asia/dreamdropsakura/reggie/filter/LoginCheckFilter.java
@@ -14,10 +14,11 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 
 /**
- * 登录请求过滤器
- * 1. 自定义一个过滤器 LoginCheckFilter 并实现 Filter 接口, 在doFilter方法中完成校验的逻
+ * 登录请求判断过滤器
+ * 1. 自定义一个过滤器 LoginCheckFilter 并实现 Filter 接口, 在doFilter方法中完成登录校验的逻
  * 辑。
  *
  * @author 童话的爱
@@ -54,12 +55,15 @@ public class LoginCheckFilter implements Filter {
         // 1. 获取本次请求的URI
         //log.info("[LoginCheckFilter]RequestURI: " + httpServletRequest.getRequestURI());
 
-        // 2. 定义不需要处理的请求路径
+        // 2. 定义不需要拦截的请求地址
+        // 注意前端点菜界面的登录请求也需要添加上
         String[] paths = new String[]{
                 "/employee/login",
                 "/employee/logout",
+                "/user/login",
+                "/user/sendMsg",
                 "/backend/**",
-                "/front/**"
+                "/front/**",
         };
 
         // 3. 进行路径匹配，判断本次请求是否需要处理
@@ -67,8 +71,9 @@ public class LoginCheckFilter implements Filter {
             boolean match = PATH_MATCHER.match(path, httpServletRequest.getRequestURI());
             // 判断请求路径是否符合paths 中所定义的路径
             if (match) {
+                log.info("[doFilter]Matched path " + path + " at URI " + httpServletRequest.getRequestURI() + "");
                 // 如果不需要处理
-                // todo 如果提前在方法体开头对ServletResponse 获取Writer 则会有可能抛出： filterChain.doFilter() throw exception:
+                // 注意如果提前在方法体开头对ServletResponse 获取Writer 则会有可能抛出： filterChain.doFilter() throw exception:
                 //  java.lang.IllegalStateException: getWriter() has already been called for this response
                 filterChain.doFilter(servletRequest, servletResponse);
                 return;
@@ -77,36 +82,29 @@ public class LoginCheckFilter implements Filter {
 
         // 判断请求是否携带有指定的session 对象
         if (httpServletRequest.getSession().getAttribute("employee") != null) {
-            // 如果有
+            // 如果有登录后台
 
             // 设置线程变量. 这行代码不可以写在if 外面，只有在确认请求确实携带有employee session 时才继续
             long employee = (Long) httpServletRequest.getSession().getAttribute("employee");
-            log.info("[LoginCheckFilter]获得的employee值：" + employee);
+            log.info("[doFilter]有后台用户登录：" + employee);
             LocalThreadVariablePoolUtil.setCurrentThreadUserid(employee);
 
             //System.out.println("用户已经登录" + httpServletRequest.getSession().getAttribute("employee"));
             filterChain.doFilter(servletRequest, servletResponse);
             return;
-        }
-        // 如果没有登录
-        httpServletResponse.getWriter().write(objectMapper.writeValueAsString(Result.error("NOTLOGIN")));
-    }
+        } else if (httpServletRequest.getSession().getAttribute("user") != null) {
+            // 如果有登录前端
 
-    /**
-     * 路径匹配，检查本次请求是否需要放行
-     *
-     * @param urls
-     * @param requestURI
-     * @return
-     */
-    public boolean check(String[] urls, String requestURI) {
-        for (String url : urls) {
-            boolean match = PATH_MATCHER.match(url, requestURI);
-            if (match) {
-                return true;
-            }
+            String user = String.valueOf(httpServletRequest.getSession().getAttribute("user"));
+            log.info("[doFilter]有前台用户登录：" + user);
+            LocalThreadVariablePoolUtil.setCurrentThreadUserid(Long.parseLong(user));
+
+            filterChain.doFilter(servletRequest, servletResponse);
         }
-        return false;
+
+        // 如果没有登录
+        // 有时会报错 java.lang.IllegalStateException: getOutputStream() has already been called for this response
+        httpServletResponse.getWriter().write(objectMapper.writeValueAsString(Result.error("NOTLOGIN")));
     }
 
     @Override
