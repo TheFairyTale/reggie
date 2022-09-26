d14a5929a3a7053fc89df14c7e6164eaca2ceb7c
TheFairyTale <986920241@qq.com>
2022-09-26 18:27:59 +0800

    设置上传文件永久存储目录及新增下载文件功能

--- src/main/java/asia/dreamdropsakura/reggie/controller/CommonController.java
+++ src/main/java/asia/dreamdropsakura/reggie/controller/CommonController.java
@@ -4,14 +4,13 @@ import asia.dreamdropsakura.reggie.common.Result;
 import com.fasterxml.jackson.annotation.ObjectIdGenerator;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.IOException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 import java.util.UUID;
 
 /**
@@ -26,6 +25,13 @@ import java.util.UUID;
 public class CommonController {
 
     /**
+     * 获取配置文件中的文件存储目录
+     *
+     */
+    @Value("${reggie.file-path}")
+    private String filePath;
+
+    /**
      * 文件上传接口
      *
      * @param file 上传来的文件 这个文件是一个临时文件，如果不转存到其他地方，则本次请求完成后该临时文件会被删除
@@ -34,6 +40,9 @@ public class CommonController {
     public Result uploadFiles(MultipartFile file) {
         if (file.isEmpty()) {
             return Result.error("传入的内容是空的");
+        }
+        if (!filePath.endsWith("/")) {
+            filePath = filePath + "/";
         }
 
         // 1. 获取原始文件名，通过原始文件名获取文件后缀
@@ -47,7 +56,7 @@ public class CommonController {
 
         // 3. 创建文件存放目录
         // 创建一个目录对象
-        File baseDirectory = new File(new File("").getAbsolutePath() + "/temp");
+        File baseDirectory = new File(filePath);
         // 判断该目录是否存在, 不存在则创建
         if (!baseDirectory.exists()) {
             log.warn(baseDirectory.mkdirs() ? "Directories path "+ baseDirectory.getPath() +" created" : "Directories create failed.");
@@ -59,6 +68,45 @@ public class CommonController {
         } catch (IOException e) {
             return Result.error(e.getMessage());
         }
+        log.info("Before return: => " + newFileName);
         return Result.success(newFileName);
+    }
+
+    /**
+     * 获取指定名称的文件, 并通过HttpServletResponse 返回
+     *
+     * @param name
+     * @param httpServletResponse
+     * @return
+     * @throws IOException
+     */
+    @GetMapping("/download")
+    public Result downloadFile(@RequestParam String name, HttpServletResponse httpServletResponse) throws IOException {
+        if (name == null || "null".equals(name)) {
+            return Result.error("无法下载指定的文件，文件名为空");
+        }
+        if (!filePath.endsWith("/")) {
+            filePath = filePath + "/";
+        }
+
+        log.info("Before find file: => " + name);
+
+        // 读指定的文件
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream(new File(filePath + name)));
+        // 获取Http响应输出流, 通过输出流将文件返回给浏览器
+        ServletOutputStream outputStream = httpServletResponse.getOutputStream();
+        // 缓冲区
+        byte[] byteBuffer = new byte[1024];
+        // bufferedInputStream.read(用于存放读取了的数据的byte数组) 返回单次已读的字节数
+        while (bufferedInputStream.read(byteBuffer) != -1) {
+            //
+            outputStream.write(byteBuffer);
+            outputStream.flush();
+        }
+
+        outputStream.close();
+        bufferedInputStream.close();
+
+        return Result.success("成功");
     }
 }
