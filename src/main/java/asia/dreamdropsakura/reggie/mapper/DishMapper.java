6b69b84fcfbae668b09370278997c66d0456e9ba
TheFairyTale <986920241@qq.com>
2022-09-26 18:39:24 +0800

    新增获取菜肴数据功能，使用连表查询

--- src/main/java/asia/dreamdropsakura/reggie/mapper/DishMapper.java
+++ src/main/java/asia/dreamdropsakura/reggie/mapper/DishMapper.java
@@ -1,7 +1,11 @@
 package asia.dreamdropsakura.reggie.mapper;
 
+import asia.dreamdropsakura.reggie.dto.DishDto;
 import asia.dreamdropsakura.reggie.entity.Dish;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+import java.util.Collection;
+import java.util.List;
 
 /**
  * 菜肴数据库表映射类
@@ -10,4 +14,13 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @since 2022-9-20
  */
 public interface DishMapper extends BaseMapper<Dish> {
+    /**
+     * 左连接查询Dish 与Category 表
+     *
+     * @param startIndex 起始索引
+     * @param pageSize 每页记录数
+     * @param name like 查询条件
+     * @return DishDto
+     */
+    List<DishDto> selectWithDishAndCategoryTable(int startIndex, int pageSize, String name);
 }
