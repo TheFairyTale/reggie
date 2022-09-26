a010f722f823201e4e92228d0cb37ee392d6caaf
TheFairyTale <986920241@qq.com>
2022-09-26 18:35:40 +0800

    菜肴数据封装类，主要用于菜品（菜肴）管理界面中的新增菜肴页面的菜品分类下拉列表的数据获取。同时也用于将从数据库中查到的Dish 与Category 表的结果集进行合并

---
+++ src/main/java/asia/dreamdropsakura/reggie/dto/DishDto.java
@@ -0,0 +1,29 @@
+package asia.dreamdropsakura.reggie.dto;
+
+import asia.dreamdropsakura.reggie.entity.Dish;
+import asia.dreamdropsakura.reggie.entity.DishFlavor;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * 新增菜肴数据封装类，用于将前端传来的一个json 数据集内包含的两个不同的实体类封装到一起
+ * 同时它也承担Dish 与Category 表的连表查询结果集封装
+ *
+ * @author 童话的爱
+ * @since 2022-9-21
+ */
+@Data
+public class DishDto extends Dish implements Serializable {
+
+    private List<DishFlavor> flavors;
+
+    private String categoryName;
+
+    @Override
+    public String toString() {
+        return super.toString() + "DishDto{" + "flavors=" + flavors + ", categoryName='" + categoryName + "'" + "}";
+    }
+}
