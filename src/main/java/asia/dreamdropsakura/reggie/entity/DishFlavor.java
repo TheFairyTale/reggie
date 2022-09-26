1a4593583f35dc71e051b351ff10ec049eddf0ec
TheFairyTale <986920241@qq.com>
2022-09-26 18:35:59 +0800

    update code

--- src/main/java/asia/dreamdropsakura/reggie/entity/DishFlavor.java
+++ src/main/java/asia/dreamdropsakura/reggie/entity/DishFlavor.java
@@ -2,6 +2,7 @@ package asia.dreamdropsakura.reggie.entity;
 
 import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableLogic;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -62,5 +63,6 @@ public class DishFlavor implements Serializable {
      * 是否删除
      *
      */
+    @TableLogic
     private Integer isDeleted;
 }
